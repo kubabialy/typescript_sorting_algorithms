17adff99267013899539c1c138f3df1aac1beabd
kuba.bialy.92@icloud.com <kuba.bialy.92@icloud.com>
2021-06-06 20:25:00 +0200

    Fix typo in bubble_sort.spec.ts

--- test/bubble_sort.spec.ts
+++ test/bubble_sort.spec.ts
@@ -4,21 +4,21 @@ import {comparator, reversedComparator} from '../src/comparator'
 
 describe('src/bubble_sort.ts', () => {
     describe('BubbleSort.sort()', () => {
-        it('should sort numbers in asceding order using bubble sort algorithm', () => {
+        it('should sort numbers in ascending order using bubble sort algorithm', () => {
             let input: Array<number> = [1, 4, 5, 2, 6]
             const bubbleSort = new BubbleSort()
             let result: Array<number> = bubbleSort.sort(input)
             expect(result).toEqual([1, 2, 4, 5, 6])
         })
 
-        it('should sort letters in asceding order using bubble sort algorithm', () => {
+        it('should sort letters in ascending order using bubble sort algorithm', () => {
             let input: Array<string> = ['c', 'b', 'd', 'a', 'e']
             const bubbleSort = new BubbleSort()
             let result: Array<string> = bubbleSort.sort(input)
             expect(result).toEqual(['a', 'b', 'c', 'd', 'e'])
         })
 
-        it('should sort arrays in asceding order', () => {
+        it('should sort arrays in ascending order', () => {
             let input: Array<Array<number>> = [[1, 2, 3], [1, 3, 6, 4], [1]]
             const bubbleSort = new BubbleSort()
             let result: Array<Array<number>> = bubbleSort.sort(input)
